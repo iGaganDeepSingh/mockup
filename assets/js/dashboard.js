e3b02c316d5e5a04b5b6872bf880df8c2a3ce780
Gagandeep Singh <gagandeep.sid@grazitti.com>
2021-04-29 01:23:00 +0530

    assets folder added

---
+++ assets/js/dashboard.js
@@ -0,0 +1,169 @@
+(function ($) {
+  'use strict';
+  $(function () {
+    var lineStatsOptions = {
+      scales: {
+        yAxes: [{
+          display: false
+        }],
+        xAxes: [{
+          display: false
+        }]
+      },
+      legend: {
+        display: false
+      },
+      elements: {
+        point: {
+          radius: 0
+        },
+        line: {
+          tension: 0
+        }
+      },
+      stepsize: 100
+    }
+    if ($("#realtime-data").length) {
+      var realtimeChartCanvas = $("#realtime-data").get(0).getContext("2d");
+      var realtimeChart = new Chart(realtimeChartCanvas, {
+        type: 'bar',
+        data: {
+          labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "15", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"],
+          datasets: [{
+              label: 'Profit',
+              data: [330, 380, 230, 400, 309, 530, 340, 330, 380, 230, 400, 250, 350, 400, 600, 500, 510, 570, 270, 390, 470, 230, 330, 250, 230, 330, 250, 280, 400, 309, 250],
+              backgroundColor: "#0f5bff",
+              borderColor: '#0f5bff',
+              borderWidth: 0
+            }
+          ]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: true,
+          layout: {
+            padding: {
+              left: 0,
+              right: 25,
+              top: 0,
+              bottom: 0
+            }
+          },
+          scales: {
+            yAxes: [{
+              display: true,
+              gridLines: {
+                display: true
+              }
+            }],
+            xAxes: [{
+              stacked: true,
+              ticks: {
+                display: true,
+                beginAtZero: true,
+                fontColor: "#354168"
+              },
+              gridLines: {
+                color: "rgba(0, 0, 0, 0)",
+                display: false
+              },
+              barPercentage: 0.5,
+            }]
+          },
+          legend: {
+            display: false
+          },
+          elements: {
+            point: {
+              radius: 0
+            }
+          }
+        }
+      });
+    }
+    if ($('#stats-line-graph-1').length) {  
+      var lineChartCanvas = $("#stats-line-graph-1").get(0).getContext("2d");
+      var gradientStrokeFill_1 = lineChartCanvas.createLinearGradient(0, 0, 0, 50);
+      gradientStrokeFill_1.addColorStop(0, 'rgba(232, 239, 250, 1)');
+      gradientStrokeFill_1.addColorStop(1, '#e8effa');
+      var lineChart = new Chart(lineChartCanvas, {
+        type: 'line',
+        data: {
+          labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7", "Day 8", "Day 9", "Day 10", "Day 11", "Day 12", "Day 13"],
+          datasets: [{
+            label: 'Profit',
+            data: [7, 6, 9, 7, 8, 6, 8, 5, 7, 8, 6, 7, 7],
+            borderColor: '#1068d9',
+            backgroundColor: gradientStrokeFill_1,
+            borderWidth: 3,
+            fill: true
+          }]
+        },
+        options: lineStatsOptions
+      });
+    }
+    if ($('#stats-line-graph-2').length) {
+      var lineChartCanvas = $("#stats-line-graph-2").get(0).getContext("2d");
+      var gradientStrokeFill_1 = lineChartCanvas.createLinearGradient(0, 0, 0, 50);
+      gradientStrokeFill_1.addColorStop(0, 'rgba(235, 246, 237, 1)');
+      gradientStrokeFill_1.addColorStop(1, '#ebf6ed');
+      var lineChart = new Chart(lineChartCanvas, {
+        type: 'line',
+        data: {
+          labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7", "Day 8", "Day 9", "Day 10", "Day 11", "Day 12", "Day 13"],
+          datasets: [{
+            label: 'Profit',
+            data: [7, 6, 8, 5, 7, 8, 6, 7, 7, 6, 9, 7, 8],
+            borderColor: '#4dad43',
+            backgroundColor: gradientStrokeFill_1,
+            borderWidth: 3,
+            fill: true
+          }]
+        },
+        options: lineStatsOptions
+      });
+    }
+    if ($('#stats-line-graph-3').length) {
+      var lineChartCanvas = $("#stats-line-graph-3").get(0).getContext("2d");
+      var gradientStrokeFill_1 = lineChartCanvas.createLinearGradient(0, 0, 0, 50);
+      gradientStrokeFill_1.addColorStop(0, 'rgba(239, 239, 254, 1)');
+      gradientStrokeFill_1.addColorStop(1, '#efeffe');
+      var lineChart = new Chart(lineChartCanvas, {
+        type: 'line',
+        data: {
+          labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7", "Day 8", "Day 9", "Day 10", "Day 11", "Day 12", "Day 13"],
+          datasets: [{
+            label: 'Profit',
+            data: [8, 6, 7, 8, 5, 7, 9, 7, 8, 7, 6, 7, 6],
+            borderColor: '#6e77f3',
+            backgroundColor: gradientStrokeFill_1,
+            borderWidth: 3,
+            fill: true
+          }]
+        },
+        options: lineStatsOptions
+      });
+    }
+    if ($('#stats-line-graph-4').length) {
+      var lineChartCanvas = $("#stats-line-graph-4").get(0).getContext("2d");
+      var gradientStrokeFill_1 = lineChartCanvas.createLinearGradient(0, 0, 0, 50);
+      gradientStrokeFill_1.addColorStop(0, 'rgba(254, 250, 239, 1)');
+      gradientStrokeFill_1.addColorStop(1, '#fefaef');
+      var lineChart = new Chart(lineChartCanvas, {
+        type: 'line',
+        data: {
+          labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7", "Day 8", "Day 9", "Day 10", "Day 11", "Day 12", "Day 13"],
+          datasets: [{
+            label: 'Profit',
+            data: [7, 6, 8, 5, 8, 6, 8, 7, 8, 6, 9, 7, 7],
+            borderColor: '#fcd156',
+            backgroundColor: gradientStrokeFill_1,
+            borderWidth: 3,
+            fill: true
+          }]
+        },
+        options: lineStatsOptions
+      });
+    }
+  });
+})(jQuery);
